aa99343b319ea964f873bcdf5115d334572139ad
Joao Frazao <jm.frazao@campus.fct.unl.pt>
2021-04-11 14:58:53 +0000

    Created a map function to import data from a file

--- jokeapp/src/App.js
+++ jokeapp/src/App.js
@@ -1,28 +1,17 @@
 import React from "react"
 import Joke from "./Joke"
-
+import jokesData from "./jokesData"
 function App(){
+    const jokeComponents = jokesData.map ((joke) => {
+        // arrow function has implicit return
+        return(
+            <Joke question = {joke.question} punchline = {joke.punchline}/>
+        )       
+    })
+
     return(
         <div>
-            <Joke 
-                question = "What’s the best thing about Switzerland?"
-                punchline = "I don’t know, but the flag is a big plus."  
-            />
-            <Joke 
-                question = "Did you hear about the mathematician who’s afraid of negative numbers?"
-                punchline = "He’ll stop at nothing to avoid them."  
-            />
-            <Joke 
-                question = "Why do we tell actors to “break a leg?”"
-                punchline = "Because every play has a cast."  
-            />
-            <Joke 
-                punchline = "Don't interrupt someone working intently on a puzzle. Chances are, you'll hear some crosswords."  
-            />
-            <Joke 
-                question = "Which country's capital has the fastest-growing population?"
-                punchline = "Ireland. Every day it's Dublin."  
-            />
+            {jokeComponents}
         </div>
     )
 }
---
+++ jokeapp/src/jokesData.js
@@ -0,0 +1,30 @@
+
+const jokesData = [
+    {
+        id: 1,
+        question: "What’s the best thing about Switzerland?",
+        punchline: "I don’t know, but the flag is a big plus."
+    },
+    {
+        id: 2,
+        question:"Did you hear about the mathematician who’s afraid of negative numbers?",
+        punchline: "He’ll stop at nothing to avoid them."
+    },
+    {
+        id: 3,
+        question:"Why do we tell actors to “break a leg?”" ,
+        punchline: "Because every play has a cast."
+    },
+    {
+        id: 4,
+        punchline: "Don't interrupt someone working intently on a puzzle. Chances are, you'll hear some crosswords."
+    },
+    {
+        id: 5,
+        question: "Which country's capital has the fastest-growing population?",
+        punchline: "Ireland. Every day it's Dublin."
+    }
+
+]
+
+export default jokesData
